c4e6a6094d8f2278e4b358095b88838458995ae6
Chinmay Duse <81753215+psyphon1@users.noreply.github.com>
2025-01-30 19:00:47 +0530

    Create largest_num.java

---
+++ DSA_Practice/largest_num.java
@@ -0,0 +1,22 @@
+public class largest_num {
+    public static void findLargest() {
+        int num[] = {2, 6, 3, 9, 54, 1};
+
+        if (num.length == 0) {
+            System.out.println("Array is empty!");
+            return;
+        }
+
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] > largest) {
+                largest = num[i];
+            }
+        }
+        System.out.println("Largest is " + largest);
+    }
+
+    public static void main(String args[]) {
+        findLargest();
+    }
+}
